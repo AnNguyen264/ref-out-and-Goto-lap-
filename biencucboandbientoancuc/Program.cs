c3ccdc89801d740bc34c544d0921f34d3e134ce9
AnNguyen(NguyenPhucAn) <npan2604work@gmail.com>
2024-07-03 21:59:38 +0700

    ref and out

---
+++ biencucboandbientoancuc/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace biencucboandbientoancuc
+{
+    internal class Program
+    {
+        static int x = 5;
+        static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine(x);
+            x = 10;
+            printsomting();
+        }
+        static void printsomting()
+        {
+            Console.WriteLine(x);
+        }*/
+
+            int values;
+            //Console.WriteLine("Values before increase: {0}",values);
+            Increasevalues(out values);
+            Console.WriteLine("Values After increase: {0}", values);
+            Console.ReadLine();
+
+        }
+        static void Increasevalues(out int count)
+        {
+             count = 0;
+            count++;
+        }
+    }
+}
